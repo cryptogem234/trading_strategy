b2ac9ee2a01888a26524f76e88cba46f590b958c
cryptogem234 <155499758+cryptogem234@users.noreply.github.com>
2024-01-04 09:24:02 -0500

    Add files via upload

--- strategies/Russell_Rat_FTLT/strategy.py
+++ strategies/Russell_Rat_FTLT/strategy.py
@@ -60,24 +60,4 @@ def execute_strategy():
 
     curr_alloc_df = alloc_df[['strategy_name','eff_date','ticker']]
 
-    # Get the directory of the current script
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    # Construct the absolute path to the CSV file
-    hist_alloc_df_path = os.path.join(current_directory, 'hist_alloc_df.csv')
-    hist_alloc_df = pd.read_csv(hist_alloc_df_path)
-
-    hist_alloc_df['eff_date'] = pd.to_datetime(hist_alloc_df['eff_date'], errors='coerce')
-    latest_hist_alloc_df = hist_alloc_df[['ticker']][hist_alloc_df['eff_date'] == hist_alloc_df['eff_date'].max()]
-
-    df_1 = curr_alloc_df[['ticker']].astype(str).reset_index(drop=True)
-    df_2 = latest_hist_alloc_df[['ticker']].astype(str).reset_index(drop=True)
-
-    if df_1.equals(df_2) != True:
-        curr_alloc_df['action'] = 'Rebalancing Required'
-        curr_alloc_df.to_csv(hist_alloc_df_path, mode='a', index=False, header=False)
-    else:
-        curr_alloc_df['action'] = 'NO Rebalancing Required'
-
-    curr_alloc_df['eff_date'] = pd.to_datetime(curr_alloc_df['eff_date']).apply(lambda x: x.strftime('%Y-%m-%d %I:%M %p'))
-
     return curr_alloc_df
