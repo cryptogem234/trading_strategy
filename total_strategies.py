876d11d2157a05764facd2a665a41b4fb07bbf98
cryptogem234 <155499758+cryptogem234@users.noreply.github.com>
2024-01-04 20:37:51 -0500

    Add files via upload

--- total_strategies.py
+++ total_strategies.py
@@ -1,7 +1,5 @@
-import pandas as pd
 
-from strategies.TQQQ_or_not_UVXY_w_SOXS import test2 as tqqq_or_not
-#from strategies.Nothing_But_Bonds import strategy as nothing_but_bonds
+from strategies.TQQQ_or_not_UVXY_w_SOXS import strategy as tqqq_or_not
 from strategies.Russell_Rat_FTLT import strategy as russell_rat_ftlt
 
 
