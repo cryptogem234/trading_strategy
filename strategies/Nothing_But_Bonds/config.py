9bf87a095ca9affe308b1d1b872fcc843a2f4b9a
cryptogem234 <155499758+cryptogem234@users.noreply.github.com>
2024-02-03 18:38:11 -0500

    Add files via upload

--- strategies/Nothing_But_Bonds/config.py
+++ strategies/Nothing_But_Bonds/config.py
@@ -2,4 +2,4 @@
 strategy_name = 'Bonds Momentum - IEF, TLT, TMF, TMV Momentums'
 sym_list = ['TMF', 'TMV', 'TQQQ', 'BND', 'TLT', 'SHV', 'SHY', 'IEF', 'UBT']
 period = '1000d'
-hist_days_list = [1, 3, 5, 8, 10, 15, 20, 21, 50, 60, 110, 200, 350, 450, 550]
+hist_days_list = [1, 3, 5, 8, 10, 15, 20, 21, 50, 60, 110, 135, 200, 350, 450, 550]
